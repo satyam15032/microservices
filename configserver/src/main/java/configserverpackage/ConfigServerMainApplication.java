db96be2700ef935612ee2114b834b3c3831b8166
satyam15032 <Satyam@27>
2022-08-31 11:46:48 +0530

    config server impl

---
+++ configserver/src/main/java/configserverpackage/ConfigServerMainApplication.java
@@ -0,0 +1,13 @@
+package configserverpackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerMainApplication.class,args);
+    }
+}
---
+++ configserver/src/test/java/configserverpackage/ConfigServerTest.java
@@ -0,0 +1,13 @@
+package configserverpackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigServerTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
